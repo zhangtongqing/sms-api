491e6718f751b6da9b8a1dfe0a0d89ddc6d3a699
zhangtongqing <798998688@qq.com>
2016-08-15 19:34:30 +0800

    测试api工程

--- src/main/java/com/miaodi/api/miaodiyun/httpApiDemo/common/Config.java
+++ src/main/java/com/miaodi/api/miaodiyun/httpApiDemo/common/Config.java
@@ -17,15 +17,15 @@ public class Config
 //	public static final String AUTH_TOKEN = "9431a041fd9d478598b2a6cd315bf688";
 //	public static final String RESP_DATA_TYPE = "json";
 	//本地模式
-	public static final String BASE_URL = "http://127.0.0.1:8079/20150822";
-	public static final String ACCOUNT_SID = "db6526e7382049b893119a0535b218b4";
-	public static final String AUTH_TOKEN = "abc2c9bbac5b4442b386b5f756437bce";
-	public static final String RESP_DATA_TYPE = "json";
+//	public static final String BASE_URL = "http://127.0.0.1:8079/20150822";
+//	public static final String ACCOUNT_SID = "db6526e7382049b893119a0535b218b4";
+//	public static final String AUTH_TOKEN = "abc2c9bbac5b4442b386b5f756437bce";
+//	public static final String RESP_DATA_TYPE = "json";
 
 	//测试环境
-//	public static final String BASE_URL = "http://113.31.65.254:8080/distributor/20150822";
-//	public static final String ACCOUNT_SID = "5d589cd2a88c45f99f0c6c87f3d98e34";
-//	public static final String AUTH_TOKEN = "154febf9f49946a2a44077c5f0ac8708";
-//	public static final String RESP_DATA_TYPE = "json";
+	public static final String BASE_URL = "http://113.31.65.254:8080/distributor/20150822";
+	public static final String ACCOUNT_SID = "5d589cd2a88c45f99f0c6c87f3d98e34";
+	public static final String AUTH_TOKEN = "154febf9f49946a2a44077c5f0ac8708";
+	public static final String RESP_DATA_TYPE = "json";
 
 }
